70e9e9fc07b4b59c04db159ebb7075e53cbf5a2a
AzamatZhanseitov <36170456+AzamatZhanseitov@users.noreply.github.com>
2018-02-06 01:21:26 +0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+var numberRow = $('#inputHeight').val();
+var numberColumn = $('#inputWeight').val();
+var currentColour = $('#colorPicker').val();
+// click event
 
 function makeGrid() {
+ $('#pixelCanvas').empty(); 
+var numberRow = $('#inputHeight').val();
+var numberColumn = $('#inputWeight').val();
+// clear existing table
 
-// Your code goes here!
-
+  // loop to add rows and columns
+  for (x = 0; x < numberRow; x++) {
+    $('#pixelCanvas').append("<tr></tr>");
+  
+  for (y=0; y < numberColumn; y++){
+          $('#pixelCanvas').children().last().append("<td></td>");
+  }
+  }
 }
+
+//execute
+$("#sizePicker").submit(function(e){
+	e.preventDefault();
+	makeGrid();
+});
